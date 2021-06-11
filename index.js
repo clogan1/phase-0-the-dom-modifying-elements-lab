d6e140fb20d583253c7671a240cdfb86debd4c05
Claire <clairelogan16@gmail.com>
2021-06-10 22:06:52 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+main.remove();
+
+//body.innerHTML = "<h1 id="victory">YOUR-NAME is the champion</h1>";
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = "Claire is the champion!";
